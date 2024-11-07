9cfdd0df180f773dd6bb6d7bff61700bdbded0c1
nquanghien97 <nquanghien97@gmail.com>
2024-11-07 23:01:10 +0700

    implement: admin

--- src/services/candidate_information.ts
+++ src/services/candidate_information.ts
@@ -1,10 +1,10 @@
 import api from "../config/api";
 
-export function getAllCandidate({ page = 1, pageSize = 10 } : { page?: number, pageSize?: number }) {
+export function getAllCandidate({ page = 1, pageSize = 10, full_name, phone_number } : { page?: number, pageSize?: number, full_name?: string, phone_number?: string }) {
   const params = new URLSearchParams();
   if (page) params.append('page', page.toString());
   if (pageSize) params.append('pageSize', pageSize.toString());
-  if (full_name) params.append('job_name', full_name.toString());
+  if (full_name) params.append('full_name', full_name.toString());
   if (phone_number) params.append('phone_number', phone_number.toString());
   return api.get(`/apply?${params.toString()}`)
 }
